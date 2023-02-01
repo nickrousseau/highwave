eae97b0b0df977fd863794880eb8b1a0858319a0
nickrousseau <rousseaunicholas@yahoo.com>
2023-01-31 16:19:23 -0800

    change

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("💚💚Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("💙💙Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
