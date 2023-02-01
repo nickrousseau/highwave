3e15bbf7872575342a6d0d7a7d1c81254713ddfd
nickrousseau <rousseaunicholas@yahoo.com>
2023-02-01 13:12:22 -0800

    change

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💙💙Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function (array){
+  for (let i = 0; i < array.length; i++){
+    return array[0];
+  }
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
+assertEqual(head([4]), 4);
