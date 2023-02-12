ab6d0298dec53c851eae09e5e52bb6eb60cfdd1e
nickrousseau <rousseaunicholas@yahoo.com>
2023-02-11 19:41:23 -0800

    changes

--- findKey.js
+++ findKey.js
@@ -77,17 +77,17 @@ console.log(
   )
 );
 
-const obj1 = findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
 );
-const obj2 = "noma";
-
-assertObjectsEqual(obj1, obj2);
