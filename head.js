f0d9656262cf106baf206a015b805302cd673c6d
nickrousseau <rousseaunicholas@yahoo.com>
2023-05-20 14:29:14 +0200

    refactored and export included

--- head.js
+++ head.js
@@ -1,18 +1,7 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💙💙Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function (array) {
   for (let i = 0; i < array.length; i++) {
     return array[0];
   }
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([4]), 4);
+module.exports = head;
